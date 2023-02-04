ab4a005d520edd064fc26d578887c7b0629de9da
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-02-04 21:49:45 +0100

    Add header component and deployed in App

---
+++ public/js/components/Header/Header.js
@@ -0,0 +1,15 @@
+import Component from "../Component/Component.js";
+export default class Header extends Component {
+    #title;
+    constructor(title, parentElement, cssClasses = "") {
+        super(parentElement, "main-header", "header");
+        this.#title = title;
+    }
+    render() {
+        super.render();
+        this.domElement.innerHTML = `
+      <img src="/pokemon-logo.svg" alt="Pokemon logo" width="300px">
+      <img src="/public/assets/img/sardiner.webp" alt="Poke espeto" width="150px">
+      <h1>${this.#title}</h1>`;
+    }
+}
--- src/components/App/App.ts
+++ src/components/App/App.ts
@@ -1,11 +1,14 @@
 import Component from "../Component/Component.js";
+import Header from "../Header/Header.js";
 
 export default class App extends Component {
   #children: Component[];
 
   constructor(parentElement: HTMLElement | undefined) {
     super(parentElement, "div");
-    this.#children = [];
+    this.#children = [
+      new Header("Pokedex oficial de Espetocoders", parentElement),
+    ];
   }
 
   render(): void {
