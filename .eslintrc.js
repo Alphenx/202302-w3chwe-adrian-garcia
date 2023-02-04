8cde7c807f2f6f69ca0ee18e5f016bccfd862340
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-02-04 11:00:18 +0100

    Add jest config, eslint rule and test

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
+    "@typescript-eslint/no-unsafe-call": "error",
     "no-unused-vars": [
       "error",
       {
--- src/components/Component/Component.test.ts
+++ src/components/Component/Component.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import Component from "./Component";
 
 describe("Given a Component component", () => {
