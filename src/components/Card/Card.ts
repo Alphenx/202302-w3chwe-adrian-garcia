422d96d04deb0795c6880408f23abf7a7853b363
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-02-05 17:56:15 +0100

    Add Card component

---
+++ src/components/Card/Card.ts
@@ -0,0 +1,27 @@
+import Component from "../Component/Component.js";
+import type PokemonTemplate from "../pokemon-types.js";
+
+export default class Card extends Component {
+  #pokemon: PokemonTemplate;
+  constructor(
+    parentElement: HTMLElement | undefined,
+    pokemon: PokemonTemplate,
+    cssClasses = "poke-cards__pokemon"
+  ) {
+    super(parentElement, cssClasses, "article");
+    this.#pokemon = pokemon;
+  }
+
+  render(): void {
+    super.render();
+    this.domElement.innerHTML = `
+     <img src="${this.#pokemon.img}" alt="${
+      this.#pokemon.pokeName
+    }" class="pokemon__img">
+          <p class="pokemon__number">#${this.#pokemon.id
+            .toString()
+            .padStart(3, "0")}</p>
+          <h4 class="pokemon__name">${this.#pokemon.pokeName}</h4>
+    `;
+  }
+}
